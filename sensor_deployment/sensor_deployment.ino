6bf0099958ee47b35ae221237fd774475c4f0be2
Kevin Wang <59260907+Togohogo1@users.noreply.github.com>
2024-03-30 21:18:11 -0400

    Create states enum

--- sensor_deployment/sensor_deployment.ino
+++ sensor_deployment/sensor_deployment.ino
@@ -1,6 +1,26 @@
+/*
+Code to control sensor deployment subsystem of the prototype.
+
+Controls a NEMA 17 stepper motor with an A4988 driver module. Uses as little external libraries as possible.
+*/
+
+typedef enum {
+  RETRACTED,
+  EXTENDING,
+  EXTENDED,
+  MEASURING,
+  DATA_SENDING,
+  RETRACTING,
+  NONE,
+  NUM_STATES
+} sensorState;
+
+// Keep track of both current and previous may be useful
+sensorState currentState = RETRACTED;
+sensorState previousState = NONE;
+
 void setup() {
 }
 
 void loop() {
 }
-
